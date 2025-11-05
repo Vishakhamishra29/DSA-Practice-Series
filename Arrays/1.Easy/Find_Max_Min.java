b84c8c4955113e2a4e8f11f8b4dffb8a06c19baf
vishakhamishra <vidhumishra2919@gmai.com>
2025-11-05 12:00:06 +0530

    Q1:Find the Max and Min element

--- Arrays/1.Easy/Find_Max_Min.java
+++ Arrays/1.Easy/Find_Max_Min.java
@@ -13,17 +13,19 @@ import java.util.Scanner;
 
 public class Find_Max_Min {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int arr[]={10,33,22,44,55,66};
+        int n=arr.length;
+        // Scanner sc = new Scanner(System.in);
 
         // Input array size
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
+        // System.out.print("Enter number of elements: ");
+        // int n = sc.nextInt();
 
-        int[] arr = new int[n];
-        System.out.println("Enter array elements: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        // int[] arr = new int[n];
+        // System.out.println("Enter array elements: ");
+        // for (int i = 0; i < n; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
 
         // Initialize min and max
         int min = arr[0];
@@ -43,6 +45,6 @@ public class Find_Max_Min {
         System.out.println("Minimum element: " + min);
         System.out.println("Maximum element: " + max);
 
-        sc.close();
+        // sc.close();
     }
 }
