a8e4c11e2494e01fec748eda1926f95090476094
vishakhamishra <vidhumishra2919@gmai.com>
2025-11-08 16:14:22 +0530

    Q3:Find the largest element in 2D array

--- Arrays/1.Easy/Multidimensional.java
+++ Arrays/1.Easy/Multidimensional.java
@@ -1,21 +1,24 @@
 import java.util.Arrays;
 public class Multidimensional {
+   //MultiDimensional Array
     public static void main (String args[]){
     int[][] arr={
-        {1,2,3,4},
-        {5,6,7,8},
+        {1,2,3},
+        {5,6},
         {9,10,11,12}
     };
-    int largest=arr[0][0];
+    //Find the largest number in the array
+    // int largest=arr[0][0];
   
     for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr[i].length;j++){
-     if(arr[i][j]>largest){
-        largest=arr[i][j];
-     }
-    
+    //  if(arr[i][j]>largest){
+    //     largest=arr[i][j];
+    //  }
+    System.out.print(arr[i][j] + " ");
        
     }}
-    System.out.println("The largest is : " + largest);
+    // System.out.println("The largest is : " + largest);
+    System.out.println(" ");
 }
 }
