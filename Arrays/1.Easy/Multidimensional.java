9251a6c5b53a1d6114900f210957f5866ee86927
vishakhamishra <vidhumishra2919@gmai.com>
2025-11-10 09:57:40 +0530

    Updated Multidimensional.java

--- Arrays/1.Easy/Multidimensional.java
+++ Arrays/1.Easy/Multidimensional.java
@@ -7,18 +7,18 @@ public class Multidimensional {
         {5,6},
         {9,10,11,12}
     };
-    //Find the largest number in the array
-    // int largest=arr[0][0];
+    //Find the largest number in the multidimensional array
+    int largest=arr[0][0];
   
     for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr[i].length;j++){
-    //  if(arr[i][j]>largest){
-    //     largest=arr[i][j];
-    //  }
-    System.out.print(arr[i][j] + " ");
+     if(arr[i][j]>largest){
+        largest=arr[i][j];
+     }
+   
        
     }}
-    // System.out.println("The largest is : " + largest);
-    System.out.println(" ");
+    System.out.println("The largest is : " + largest);
+   
 }
 }
